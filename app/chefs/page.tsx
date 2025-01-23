eb92e26f1c6fed54d0c1e9987fc5534081f2ad96
Subhan-anwer <sheikhanwer30@gmail.com>
2025-01-23 12:13:12 +0500

    chefs page update

--- app/chefs/page.tsx
+++ app/chefs/page.tsx
@@ -10,20 +10,20 @@ const page = () => {
 
             <section className='w-full h-auto flex items-center justify-center justify-self-center'>
                 <div className='max-w-[1320px] w-full min-h-[1386px] py-[120px] gap-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 '>
-                    <ChefCard img="1.jpg" name="Tahmina Rumi" />
-                    <ChefCard img="2.jpg" name="Jorina Begum" />
-                    <ChefCard img="3.jpg" name="M.Mohammad" />
-                    <ChefCard img="4.jpg" name="Tahmina Rumi" />
+                    <ChefCard img="chef1.jpeg" name="Tahmina Rumi" />
+                    <ChefCard img="chef2.jpg" name="Jorina Begum" />
+                    <ChefCard img="chef3.jpg" name="M.Mohammad" />
+                    <ChefCard img="chef4.jpg" name="Tahmina Rumi" />
 
-                    <ChefCard img="5.jpg" name="Tahmina Rumi" />
-                    <ChefCard img="6.jpg" name="Bisnu devgon" />
-                    <ChefCard img="7.jpg" name="Motin Molladsf" />
-                    <ChefCard img="8.jpg" name="William Rumi" />
+                    <ChefCard img="chef5.jpg" name="Tahmina Rumi" />
+                    <ChefCard img="chef6.jpg" name="Bisnu devgon" />
+                    <ChefCard img="chef7.jpg" name="Motin Molladsf" />
+                    <ChefCard img="chef8.jpg" name="William Rumi" />
 
-                    <ChefCard img="9.jpg" name="Kets william roy" />
-                    <ChefCard img="10.jpg" name="Mahmud kholil" />
-                    <ChefCard img="11.jpg" name="Ataur Rahman" />
-                    <ChefCard img="12.jpg" name="Monalisa holly" />
+                    <ChefCard img="chef9.jpg" name="Kets william roy" />
+                    <ChefCard img="chef10.jpg" name="Mahmud kholil" />
+                    <ChefCard img="chef11.jpg" name="Ataur Rahman" />
+                    <ChefCard img="chef12.jpg" name="Monalisa holly" />
                 </div>
             </section>
         </div>
